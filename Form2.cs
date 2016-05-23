0c431a1848c941cf33802308d41659f2f8496e1c
Shreyas S <shreyasshetty29@gmail.com>
2016-05-23 15:36:18 +0530

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dBDataSet.Item' table. You can move, or remove it, as needed.
+            this.itemTableAdapter.Fill(this.dBDataSet.Item);
+
+        }
+    }
+}
